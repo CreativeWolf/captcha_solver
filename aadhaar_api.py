d5ccc27be81c84d38a94db51be6fa46ddbd21737
deepu <dishednramishra>
2017-11-13 03:37:52 +0530

    added return

--- aadhaar_api.py
+++ aadhaar_api.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from subprocess import call
+from subprocess import call, PIPE
 from time import sleep
 from os import remove
 import cv2
@@ -46,11 +46,11 @@ class aadhaar_api():
 	def check_captcha(self,path):
 		f = open(path,"r")
 		self.captcha_text = f.read().strip()
-		print self.captcha_text
+		# print self.captcha_text
 		
 		if self.captcha_text.isdigit():
 			f.close()
-			print "captcha: ",self.captcha_text
+			# print "captcha: ",self.captcha_text
 			return True
 		f.close()
 		return False 
@@ -62,7 +62,7 @@ class aadhaar_api():
 
 		self.save_screenshot_of_element(img)
 		call(["tesseract","thresholded.jpg","word"])
-		print "Initail files saved"
+		# print "Initail files saved"
 		sleep(0.1)
 
 		while not self.check_captcha("word.txt"):
@@ -94,6 +94,11 @@ class aadhaar_api():
 		result = self.browser.find_elements_by_tag_name('h2')[2]
 		# result = self.browser.find_element_by_xpath('//h2[@class="floatLeft marginTop50 fontSize15 fontWeightBold color333333 textAlignCenter width100"]')
 		print result.text
+		if result.text.find("Exists") != -1:
+			print "True"
+			return True
+		print "False"
+		return False
 
 
 	def close_browser(self):
@@ -119,9 +124,13 @@ class aadhaar_api():
 
 
 
-# if __name__ == "__main__":
-API = aadhaar_api()
-API.get_captcha_text()
-API.authenticate_aadhaar("291560455557")
-	# API.init_clean()
+if __name__ == "__main__":
+	API = aadhaar_api()
+	API.get_captcha_text()
+	API.authenticate_aadhaar("291560455557")
+	API.get_captcha_text()
+	API.authenticate_aadhaar("952489062216")
+	API.get_captcha_text()
+	API.authenticate_aadhaar("952489452216")
+	API.init_clean()
 	# API. close_browser()
